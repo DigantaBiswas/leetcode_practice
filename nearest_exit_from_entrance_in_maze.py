61851e826452cfdc677eb8d244a23ceeeb43a0e9
diagnta <digantaprotik@gmail.com>
2022-12-16 08:15:16 +0600

    done

--- nearest_exit_from_entrance_in_maze.py
+++ nearest_exit_from_entrance_in_maze.py
@@ -9,7 +9,7 @@ class Solution:
         adjacent_indices = []
         if i > 0:
             adjacent_indices.append((i - 1, j))
-        if i != 0 and i + 1 < m + 1:
+        if i + 1 < m + 1:
             adjacent_indices.append((i + 1, j))
         if j > 0:
             adjacent_indices.append((i, j - 1))
@@ -21,29 +21,30 @@ class Solution:
         for position in positions:
             if position not in self.explored_for_mapping:
                 self.explored_for_mapping.append(tuple(position))
-                next_points = self.get_adjacent_indices(position[0], position[1], self.m + 1, self.n + 1)
+                next_points = self.get_adjacent_indices(position[0], position[1], self.m, self.n )
                 self.index_map.update({
                     tuple(position): next_points
                 })
                 if next_points:
                     self.populate_adjecent_node_dict(next_points)
 
-    def get_exit_indexes(self, maze):
+    def get_exit_indexes(self, maze, entrance):
         border_indexes = []
         for row_index, row_value in enumerate(maze):
             for column_index, column_value in enumerate(row_value):
-                if column_value == ".":
-                    if row_index == 0 or row_index == self.m:
-                        border_indexes.append((row_index, column_index))
-                    elif column_index == self.n or column_index == 0:
-                        border_indexes.append((row_index, column_index))
+                if  (row_index, column_index) != entrance:
+                    if column_value == ".":
+                        if row_index == 0 or row_index == self.m:
+                            border_indexes.append((row_index, column_index))
+                        elif column_index == self.n or column_index == 0:
+                            border_indexes.append((row_index, column_index))
         return border_indexes
 
     def nearest_exit(self, maze, entrance):
         self.m = len(maze) - 1
         self.n = len(maze[0]) - 1
 
-        border_list = self.get_exit_indexes(maze)
+        border_list = self.get_exit_indexes(maze, tuple(entrance))
         self.populate_adjecent_node_dict([entrance])
 
         path_list_to_borders = {}
@@ -70,10 +71,11 @@ class Solution:
                         path_remaining.append(adjecent_cell)
         final_step_counts = [value for key, value in path_list_to_borders.items()]
         if final_step_counts:
-            min = min(final_step_counts)
+           print(min(final_step_counts))
         else:
             print("-1")
 
 
 
-Solution().nearest_exit([["+","+",".","+"],[".",".",".","+"],["+","+","+","."]], [1,2])
+Solution().nearest_exit([["+",".","+","+","+","+","+"],["+",".","+",".",".",".","+"],["+",".","+",".","+",".","+"],["+",".",".",".","+",".","+"],["+","+","+","+","+",".","+"]]
+,[0,1])
